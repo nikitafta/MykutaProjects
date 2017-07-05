985341b3533e5d2bb90132d84b1476e897e33597
NAzarMh <nazar.myhailiv@gmail.com>
2017-07-05 19:59:28 +0300

    Some line

--- src/lesson2/task0101.java
+++ src/lesson2/task0101.java
@@ -3,5 +3,6 @@ package lesson2;
 public class task0101 {
     public static void main(String[] args) {
         System.out.println("git axammple");
+        System.out.println("New code line");
     }
 }
